d6fe61926932ad58c861601eb581f055b6a06639
smahm01 <sadek.mahmood@mail.mcgill.ca>
2025-01-19 18:59:04 -0500

    sync

--- frontend/components/AddItemShoppingList.js
+++ frontend/components/AddItemShoppingList.js
@@ -34,18 +34,18 @@ export const AddItemShoppingList = ({ visible, onClose, onAddItem }) => {
 
             const products = response.data.products.filter(product => product.lang === "en");
             
-            for (let i = 0; i < products.length; i++) {
-                console.log(products[i].id);
-                console.log(products[i].product_name);
-                console.log(products[i].brands);
-                console.log(products[i].categories);
-                console.log(products[i].allergens);
-                console.log(products[i].nutriments.proteins_100g);
-                console.log(products[i].nutriments.fat_100g);
-                console.log(products[i].nutriments.carbohydrates_100g);
-                console.log(products[i].nutriments["energy-kcal_100g"]);
-                console.log(products[i].image_url);
-            }
+            // for (let i = 0; i < products.length; i++) {
+            //     console.log(products[i].id);
+            //     console.log(products[i].product_name);
+            //     console.log(products[i].brands);
+            //     console.log(products[i].categories);
+            //     console.log(products[i].allergens);
+            //     console.log(products[i].nutriments.proteins_100g);
+            //     console.log(products[i].nutriments.fat_100g);
+            //     console.log(products[i].nutriments.carbohydrates_100g);
+            //     console.log(products[i].nutriments["energy-kcal_100g"]);
+            //     console.log(products[i].image_url);
+            // }
 
             setProducts(products);
 
@@ -75,10 +75,11 @@ export const AddItemShoppingList = ({ visible, onClose, onAddItem }) => {
                         data={products}
                         keyExtractor={(item) => item.id}
                         renderItem={({ item }) => (
-                            <ScannedItemDisplayCard 
+                            <ScannedItemDisplayCard
+                                key={item.id} 
                                 productName={item.product_name}
                                 productBrand={item.brands}
-                                categories={item.categories}
+                                categories={item.categories || "Unknown"}
                                 allergens={item.allergens}
                                 protein={item.nutriments.proteins_100g}
                                 fat={item.nutriments.fat_100g}
